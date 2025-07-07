81789da65afb05f77798e4f2f66f36f0a17ce23e
kitlisn <130925023+kitlisn@users.noreply.github.com>
2025-07-07 20:22:37 +0300

    Update train_with_mlflow.py

--- src/train_with_mlflow.py
+++ src/train_with_mlflow.py
@@ -13,15 +13,30 @@ def main(data_path: str = "data/5k.csv") -> None:
     X, y = preprocess(df)
 
     with mlflow.start_run():
-        model, X_train, y_train, X_test, y_test = train_model(X, y)
+        model, X_train, y_train, X_test, y_test = train_model(
+            X,
+            y,
+            iterations=1000,
+            learning_rate=0.08,
+            depth=6,
+            l2_leaf_reg=5.0,
+            bagging_temperature=0.2,
+            random_strength=5.0,
+            class_weights={0: 1, 1: 2, 2: 2.5},
+        )
+
 
         report, cm = evaluate_model(model, X_test, y_test)
         print("Classification report:\n", report)
         print("Confusion matrix:\n", cm)
 
-        mlflow.log_param("iterations", 200)
+        mlflow.log_param("iterations", 1000)
         mlflow.log_param("depth", 6)
-        mlflow.log_param("learning_rate", 0.1)
+        mlflow.log_param("learning_rate", 0.08)
+        mlflow.log_param("l2_leaf_reg", 5.0)
+        mlflow.log_param("bagging_temperature", 0.2)
+        mlflow.log_param("random_strength", 5.0)
+        mlflow.log_param("class_weights", "{0:1,1:2,2:2.5}")
         mlflow.log_param("model_type", "CatBoostClassifier")
 
         y_proba = model.predict_proba(X_test)
